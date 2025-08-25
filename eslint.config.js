fc00cdc2d26fc3b30efe9c3ffed08f269894116e
SOLARCHIN <NICHOLAS.AMAD.LEE@GMAIL.COM>
2025-08-25 12:46:23 -0400

    fix(next): mark client components and add eslint flat config ignores

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "node_modules",
+      ".next",
+      "dist",
+      "out",
+      "**/*.d.ts",
+    ],
+  },
+];
